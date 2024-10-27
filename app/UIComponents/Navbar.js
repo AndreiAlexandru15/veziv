abaa6b958c74ee1771993ca627341f68b1eda763
Andrei <andreibolojan539@gmail.com>
2024-10-27 17:34:06 +0200

    Navbar added

---
+++ app/UIComponents/Navbar.js
@@ -0,0 +1,81 @@
+"use client";
+
+import { useState } from "react";
+import Link from "next/link";
+import { PlusIcon } from "lucide-react";
+import { Button } from "@/components/ui/button";
+
+const Navbar = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  return (
+    <nav className="bg-white text-black shadow-sm fixed w-full z-10">
+      <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+        <div className="flex justify-between items-center h-16">
+          {/* Logo */}
+          <Link href="/" className="text-black font-bold text-xl">
+            Artistry
+          </Link>
+
+          <div className="hidden md:flex">
+            <Button>
+              <PlusIcon className="text-white" />
+            </Button>
+          </div>
+
+          {/* Mobile Menu Button */}
+          <div className="flex md:hidden">
+            <button
+              onClick={() => setIsOpen(!isOpen)}
+              className="text-black focus:outline-none"
+            >
+              <svg
+                className="w-6 h-6"
+                fill="none"
+                stroke="currentColor"
+                viewBox="0 0 24 24"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                {isOpen ? (
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth="2"
+                    d="M6 18L18 6M6 6l12 12"
+                  />
+                ) : (
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth="2"
+                    d="M4 6h16M4 12h16m-7 6h7"
+                  />
+                )}
+              </svg>
+            </button>
+          </div>
+        </div>
+      </div>
+
+      {/* Mobile Menu */}
+      {isOpen && (
+        <div className="md:hidden px-2 pt-2 pb-3 space-y-1">
+          <Link
+            href="/"
+            className="block px-3 py-2 rounded-md text-sm font-semibold "
+          >
+            Home
+          </Link>
+          <Link
+            href="/projects/new"
+            className="block px-3 py-2 rounded-md text-sm font-semibold"
+          >
+            Add New Project
+          </Link>
+        </div>
+      )}
+    </nav>
+  );
+};
+
+export default Navbar;
